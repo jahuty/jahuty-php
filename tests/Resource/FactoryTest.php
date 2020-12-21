43026551b11073dc719074a4e7eb12872c644a8f
Jack Clayton <clayjs0@gmail.com>
2020-12-20 22:38:22 -0700

    Refactor resource factory to negotiate resource to return

--- tests/Resource/FactoryTest.php
+++ tests/Resource/FactoryTest.php
@@ -2,20 +2,61 @@
 
 namespace Jahuty\Resource;
 
+use GuzzleHttp\Psr7\Response;
+use Jahuty\Action\Show;
+
 class FactoryTest extends \PHPUnit\Framework\TestCase
 {
-    public function testNewThrowsExceptionWhenNameDoesNotExist(): void
+    public function testNewThrowsExceptionWhenResponseIsUnexpected(): void
     {
         $this->expectException(\OutOfBoundsException::class);
 
-        (new Factory())->new('foo', []);
+        $action = new Show('render', 1);
+
+        // Use a response with an error status code but without the
+        // application/problem+json content type.
+        $response = new Response(500, [], "<p>Fatal error</p>");
+
+        (new Factory())->new($action, $response);
     }
 
-    public function testNewReturnsResourceWhenNameDoesExist(): void
+    public function testNewReturnsProblemWhenResponseIsProblem(): void
     {
-        $this->assertInstanceOf(
-            Render::class,
-            (new Factory())->new('render', ['content' => 'foo'])
+        $action = new Show('render', 1);
+
+        $response = new Response(
+            500,
+            ['Content-Type' => 'application/problem+json'],
+            '{ "status": 500, "type": "foo", "detail": "bar" }'
         );
+
+        $resource = (new Factory())->new($action, $response);
+
+        $this->assertInstanceOf(Problem::class, $resource);
+    }
+
+    public function testNewThrowsExceptionWhenResourceDoesNotExist(): void
+    {
+        $this->expectException(\OutOfBoundsException::class);
+
+        // An action on a non-existant resource.
+        $action = new Show('foo', 1);
+
+        (new Factory())->new($action, new Response(200));
+    }
+
+    public function testNewReturnsResource(): void
+    {
+        $action = new Show('render', 1);
+
+        $response = new Response(
+            200,
+            [],
+            '{ "content": "foo" }'
+        );
+
+        $resource = (new Factory())->new($action, $response);
+
+        $this->assertInstanceOf(Render::class, $resource);
     }
 }

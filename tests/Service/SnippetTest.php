492518f71daad595b10e469f20f7e1d0c1df44f0
Jack Clayton <clayjs0@gmail.com>
2021-03-04 21:37:50 -0700

    Move caching to snippet service

--- tests/Service/SnippetTest.php
+++ tests/Service/SnippetTest.php
@@ -6,84 +6,56 @@ use Jahuty\Action\{Index, Show};
 use Jahuty\Cache\Ttl;
 use Jahuty\Client;
 use Jahuty\Resource\Render;
+use Psr\SimpleCache\CacheInterface;
 
 class SnippetTest extends \PHPUnit\Framework\TestCase
 {
-    public function testRenderWhenParamsDoNotExist(): void
+    public function testAllRenders(): void
     {
-        $action = new Show('render', 1);
+        // a collection of renders
+        $render     = new Render(1, 'foo');
+        $collection = [$render];
 
-        $render = new Render(1, 'foo');
-
+        // a successful api request
         $client = $this->createMock(Client::class);
-        $client->expects($this->once())
-            ->method('fetch')
-            ->with($this->equalTo($action), $this->isInstanceOf(Ttl::class))
-            ->will($this->returnValue($render));
+        $client->method('request')->willReturn($collection);
 
-        (new Snippet($client))->render(1);
-    }
+        // mock a cache miss and expect a write
+        $cache = $this->createMock(CacheInterface::class);
+        $cache->method('get')->willReturn(null);
+        $cache->expects($this->once())
+            ->method('set')
+            ->with(
+                $this->matchesRegularExpression('/jahuty_[a-z0-9]+/'),
+                $this->equalTo($render),
+                $this->equalTo(null)
+            );
 
-    public function testRenderWhenParamsDoExist(): void
-    {
-        $action = new Show('render', 1, ['params' => '{"foo":"bar"}']);
+        $service = new Snippet($client, $cache, new Ttl());
 
-        $render = new Render(1, 'foo');
-
-        $client = $this->createMock(Client::class);
-        $client->expects($this->once())
-            ->method('fetch')
-            ->with($this->equalTo($action), $this->isInstanceOf(Ttl::class))
-            ->will($this->returnValue($render));
-
-        (new Snippet($client))->render(1, ['params' => ['foo' => 'bar']]);
-    }
-
-    public function testRenderWhenParamsAndTllDoExist(): void
-    {
-        $action = new Show('render', 1, ['params' => '{"foo":"bar"}']);
-
-        $render = new Render(1, 'foo');
-
-        $client = $this->createMock(Client::class);
-        $client->expects($this->once())
-            ->method('fetch')
-            ->with($this->equalTo($action), $this->equalTo(new Ttl(60)))
-            ->will($this->returnValue($render));
-
-        (new Snippet($client))->render(1, [
-            'params' => ['foo' => 'bar'],
-            'ttl'    => 60
-        ]);
-    }
-
-    public function testRendersWhenParamsDoNotExist(): void
-    {
-        $action = new Index('render', ['tag' => 'foo']);
-
-        $client = $this->createMock(Client::class);
-        $client->expects($this->once())
-            ->method('fetch')
-            ->with($this->equalTo($action))
-            ->will($this->returnValue([]));
-
-        (new Snippet($client))->renders('foo');
+        $service->allRenders('foo');
     }
 
     public function testRendersWhenParamsDoExist(): void
     {
+        // the expected action, note the json-encoded params)
         $action = new Index('render', [
             'tag'    => 'foo',
             'params' => '{"*":{"foo":"bar"},"1":{"foo":"baz"}}'
         ]);
 
+        // a no-op cache
+        $cache = $this->createMock(CacheInterface::class);
+
         $client = $this->createMock(Client::class);
         $client->expects($this->once())
-            ->method('fetch')
+            ->method('request')
             ->with($this->equalTo($action))
             ->will($this->returnValue([]));
 
-        (new Snippet($client))->renders('foo', [
+        $service = new Snippet($client, $cache, new Ttl());
+
+        $service->allRenders('foo', [
             'params' => [
                 '*' => [
                     'foo' => 'bar'
@@ -93,5 +65,102 @@ class SnippetTest extends \PHPUnit\Framework\TestCase
                 ]
             ]
         ]);
+    }
+
+    public function testRenderReturnsRenderWhenCacheMiss(): void
+    {
+        // the action to request
+        $action = new Show('render', 1);
+
+        // the render to return
+        $render = new Render(1, 'foo');
+
+        // a cache miss
+        $cache = $this->createMock(CacheInterface::class);
+
+        // a successful api request
+        $client = $this->createMock(Client::class);
+        $client->method('request')->willReturn($render);
+
+        // a cache write
+        $cache->expects($this->once())
+            ->method('set')
+            ->with(
+                $this->matchesRegularExpression('/jahuty_[a-z0-9]+/'),
+                $this->equalTo($render),
+                $this->equalTo(null)
+            );
+
+        // the service-under-test
+        $service = new Snippet($client, $cache, new Ttl());
+
+        $this->assertSame($render, $service->render(1));
+    }
+
+    public function testRenderReturnsRenderWhenCacheHit(): void
+    {
+        // the cached render
+        $render = new Render(1, 'foo');
+
+        // a cache hit
+        $cache = $this->createMock(CacheInterface::class);
+        $cache->method('get')->willReturn($render);
+
+        // a no-op client
+        $client = $this->createMock(Client::class);
+
+        // the service-under-test
+        $service = new Snippet($client, $cache, new Ttl());
+
+        $this->assertSame($render, $service->render(1));
+    }
+
+    public function testRenderWhenParamsExist(): void
+    {
+        // the expected action, note the json-formatted params
+        $action = new Show('render', 1, ['params' => '{"foo":"bar"}']);
+
+        // the render to return
+        $render = new Render(1, 'foo');
+
+        // a cache miss
+        $cache = $this->createMock(CacheInterface::class);
+
+        $client = $this->createMock(Client::class);
+        $client->expects($this->once())
+            ->method('request')
+            ->with($this->equalTo($action))
+            ->will($this->returnValue($render));
+
+        $service = new Snippet($client, $cache, new Ttl());
+
+        $service->render(1, ['params' => ['foo' => 'bar']]);
+    }
+
+    public function testRenderWhenTllExists(): void
+    {
+        $ttl = 60;
+
+        // the render to return
+        $render = new Render(1, 'foo');
+
+        // a successful api request
+        $client = $this->createMock(Client::class);
+        $client->method('request')->willReturn($render);
+
+        // mock a cache miss and expect write to use the ttl argument
+        $cache = $this->createMock(CacheInterface::class);
+        $cache->method('get')->willReturn(null);
+        $cache->expects($this->once())
+            ->method('set')
+            ->with(
+                $this->matchesRegularExpression('/jahuty_[a-z0-9]+/'),
+                $this->equalTo($render),
+                $this->equalTo($ttl)
+            );
+
+        $service = new Snippet($client, $cache, new Ttl(120));
+
+        $service->render(1, ['ttl' => $ttl]);
     }
 }
